a533ba50da13c781830953eb4602a79d11ebc13a
USER_NAME <EMAIL_ADDRESS>
2018-04-29 13:06:27 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
